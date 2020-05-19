6ed6b2405d7cf088158b25407e17c108d51d1c96
AnkilP <a293pate@uwaterloo.ca>
2020-05-19 01:53:48 -0400

    firebase shenanigans

--- idgen/go.mod
+++ idgen/go.mod
@@ -3,9 +3,12 @@ module midnight
 go 1.13
 
 require (
+	cloud.google.com/go/firestore v1.2.0 // indirect
+	firebase.google.com/go v3.12.1+incompatible
 	github.com/AnkilP/midnight v0.0.0-20200518190113-b7fe5a58fd0d
 	github.com/google/uuid v1.1.1
 	golang.org/x/net v0.0.0-20200513185701-a91f0712d120
+	google.golang.org/api v0.24.0
 	google.golang.org/grpc v1.29.1
 	google.golang.org/protobuf v1.23.0 // indirect
 )
--- idgen/main.go
+++ idgen/main.go
@@ -1,88 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"net"
-	"os"
-	"sync"
 
-	"github.com/AnkilP/midnight/idgen_proto"
-	"github.com/AnkilP/midnight/proto"
+	"golang.org/x/net/context"
 
-	guuid "github.com/google/uuid"
+	firebase "firebase.google.com/go"
 
-	context "golang.org/x/net/context"
-	grpc "google.golang.org/grpc"
-	glog "google.golang.org/grpc/grpclog"
+	"google.golang.org/api/option"
 )
 
-var grpcLog glog.LoggerV2
-
-func init() {
-	grpcLog = glog.NewLoggerV2(os.Stdout, os.Stdout, os.Stdout)
-}
-
-type Connection struct {
-	stream idgen_proto.Broadcast_RecvMessagesServer
-	id     string
-	active bool
-	error  chan error
-}
-
-type ID_GEN struct {
-	Connection []*Connection
-}
-
-func genUUID() int {
-	id := guuid.New()
-}
-
-func (s *ID_GEN) idrequestserve(ctx context.Context, msg *idgen_proto.Close) (*proto.Close, error) {
-	wait := sync.WaitGroup{}
-	done := make(chan int)
-
-	for _, conn := range s.Connection {
-		wait.Add(1)
-
-		go func(msg *proto.Message, conn *Connection) {
-			defer wait.Done()
-
-			if conn.active {
-				err := conn.stream.Send(msg)
-				grpcLog.Info("Sending message to: ", conn.stream)
-
-				if err != nil {
-					grpcLog.Errorf("Error with Stream: %v - Error: %v", conn.stream, err)
-					conn.active = false
-					conn.error <- err
-				}
-			}
-		}(msg, conn)
-
-	}
-
-	go func() {
-		wait.Wait()
-		close(done)
-	}()
-
-	<-done
-	return &proto.Close{}, nil
-}
-
 func main() {
-	var connections []*Connection
-
-	server := &Server{connections}
-
-	grpcServer := grpc.NewServer()
-	listener, err := net.Listen("tcp", ":8080")
+	opt := option.WithCredentialsFile("../keys_DO_NOT_MODIFY/midnight-31686-firebase-adminsdk-obui8-fe0e06993f.json")
+	midnightAuth, err := firebase.NewApp(context.Background(), nil, opt)
 	if err != nil {
-		log.Fatalf("error creating the server %v", err)
+		fmt.Errorf("error initializing app: %v", err)
 	}
 
-	grpcLog.Info("Starting server at port :8080")
+	defaultClient, err := midnightAuth.Auth(context.Background())
+	if err != nil {
+		log.Fatalf("error getting Auth client: %v\n", err)
+	}
 
-	proto.RegisterBroadcastServer(grpcServer, server)
-	grpcServer.Serve(listener)
 }
